8613b1ff895337e398ab4af2594eb1d0558bcdb8
HONG NHAN <nguyenhongnhan9a5@gmail.com>
2024-09-06 16:14:53 +0700

    Updated lessons

--- lesson13.c
+++ lesson13.c
@@ -7,7 +7,15 @@ int main (int argc, char *argv[]) {
     FILE *theFile = fopen("lesson13File.txt", "w");
     fprintf(theFile, "This is testing the file by using c");
     fclose(theFile);
-    //
-    //
+    /*
+    if(remove("lesson13File.txt") ==0){
+        printf("The file was deleted.\n");
+    }
+    else{
+        printf("No file was deleted.\n");
+    }
+    */
+    
+
     return 0;
 }
