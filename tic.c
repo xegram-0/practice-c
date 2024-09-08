481b94f4ca439c54470ada9974681e1b6fe47cc5
HONG NHAN <nguyenhongnhan9a5@gmail.com>
2024-09-09 06:51:07 +0700

    Updated codes

--- tic.c
+++ tic.c
@@ -1,5 +1,44 @@
-//
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+char PLAYER = 'X';
+char COMPUTER = 'O';
+
+// smaller funcitons
+void resetBoard;
+void board;
+void winner;
+void checkingLine;
+void checkingWinner;
+int playerMove;
+// main funtion
 int main() {
-    
+    char board[3][3];
+    bool win = 0;
+    resetBoard(board);
+    do {
+        /* code here */ 
+        playerMove;
+        win = checkingLine;
+
+    } while(win == 0);
+
+
     return 0;
 }
+
+void resetBoard(char board[]){
+
+    return 0;
+}
+int playerMove;
+void board;
+void winner
+int checkingLine{
+
+    return winning;
+}
+void checkingWinner;
